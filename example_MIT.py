16ecbce8c5d0aa02d867582ddaa372be057cea28
Jon Turner <jon.turner@ga4gh.org>
2026-07-16 13:18:26 +0100

    Delete example_MIT.py

--- example_MIT.py
+++
@@ -1,4 +0,0 @@
-# SPDX-FileCopyrightText: 2021-2026 Your Org / GA4GH Contributors
-# SPDX-License-Identifier: MIT
-
-"""Example: files you author yourself go under your project's primary license (MIT here)."""
