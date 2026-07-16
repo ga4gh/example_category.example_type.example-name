b0197c4398748eccc4afd48a52b1d5cb1eecd386
Jon Turner <jon.turner@ga4gh.org>
2026-07-16 13:14:05 +0100

    Add files via upload

---
+++ src/example_MIT.py
@@ -0,0 +1,4 @@
+# SPDX-FileCopyrightText: 2021-2026 Your Org / GA4GH Contributors
+# SPDX-License-Identifier: MIT
+
+"""Example: files you author yourself go under your project's primary license (MIT here)."""
